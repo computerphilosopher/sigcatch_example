f6a4b3c91c2a603b80c5ce4021aaa844d35410b8
computerphilosopher <erlk0nig@naver.com>
2021-02-02 15:26:54 +0900

    modify malloc code

--- malloc.c
+++ malloc.c
@@ -2,8 +2,36 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <signal.h>
+
+void sig_term_handler(int signum, siginfo_t *info, void *ptr)
+{
+    int count = 0;
+    while (1) {
+        printf("ignore SIGTERM (%ds)\n", count++);
+        sleep(1);
+
+        if (count > 3000) {
+            break;
+        }
+    }
+}
+
+void catch_sigterm()
+{
+    static struct sigaction _sigact;
+
+    memset(&_sigact, 0, sizeof(_sigact));
+    _sigact.sa_sigaction = sig_term_handler;
+    _sigact.sa_flags = SA_SIGINFO;
+
+    sigaction(SIGTERM, &_sigact, NULL);
+}
+
 
 int main() {
+    catch_sigterm();
+
     int *ptr = NULL;
     int gb = 1024 * 1024 * 1024;
 
@@ -12,4 +40,6 @@ int main() {
         memset(ptr, 0, 0);
         sleep(5);
     }
+    
+    return 0;
 }
